aaba1578aa042f94744eca3df27637c8306ba5cd
juan arrieta <caporo1974@yahoo.es>
2022-10-05 19:30:41 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/Interface/CategoryInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.Inteface;
+
+import com.example.demo.Modelo.Category;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Juan Carlos Arrieta Bustos
+ */
+public interface CategoryInterface extends CrudRepository<Category, Integer > {
+    
+}
